25c657c9f7c73d5d7611c86158f706322782c40e
Alex Teisheira <fukirx@gmail.com>
2020-08-14 17:03:10 +0300

    chat layout

--- src/components/ChatRoom/ChatRoom.js
+++ src/components/ChatRoom/ChatRoom.js
@@ -1,35 +1,69 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import { connect } from "react-redux";
+import { useDidMountEffect } from "../customHooks/didMountEffect";
 import "./ChatRoom.sass";
 
-import camera from "../../resources/img/camera.png";
 import arrow from "../../resources/img/arrow.png";
 
-const ChatRoom = props => (
-  <div className="chat-room">
-    <div className="header">
-      <h2>Testing Task</h2>
-    </div>
-    <div className="main">
-      <div className="bgc-top"></div>
-    </div>
-    <div className="footer">
-      <i className="fas fa-trash-alt"></i>
-      <input type="text" placeholder="Type message. . ." />
-      <div className="send">
-        <img src={arrow} width="20px" alt="" />
+const ChatRoom = ({ list, message, onChangeMessage, onPost, onClear }) => {
+  const chat = useRef(null);
+
+  useDidMountEffect(() => {
+    chat.current.scrollTop = chat.current.scrollHeight;
+  }, [list.length]);
+
+  const renderList = () => {
+    return list.map((item, i) => {
+      return (
+        <li key={i} className={item.user === "kemer" ? "user-item" : null}>
+          <p>{item.body}</p>
+          <span style={item.user === "kemer" ? { textAlign: "right" } : null}>
+            {item.user}
+          </span>
+        </li>
+      );
+    });
+  };
+
+  return (
+    <div className="chat-room">
+      <div className="header">
+        <h2>Testing Task</h2>
+      </div>
+      <div
+        className="main"
+        onWheel={e => (chat.current.scrollTop += e.deltaY * 0.3)}
+        ref={chat}
+      >
+        <ul>{renderList()}</ul>
+        <div className="bgc-top"></div>
+      </div>
+      <div className="footer">
+        <i className="fas fa-trash-alt" onClick={onClear}></i>
+        <input
+          type="text"
+          placeholder="Type message. . ."
+          value={message}
+          onChange={e => onChangeMessage(e.target.value)}
+        />
+        <div className="send" onClick={onPost}>
+          <img src={arrow} width="20px" alt="" />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
-const mapStateToProps = state => ({
-  // blabla: state.blabla,
+const mapStateToProps = ({ chat, message }) => ({
+  list: chat,
+  message
 });
 
 const mapDispatchToProps = dispatch => ({
-  // fnBlaBla: () => dispatch(action.name()),
+  onChangeMessage: text => dispatch({ type: "CHANGE_MESSAGE", payload: text }),
+  onPost: () => dispatch("POST_MESSAGE"),
+  onClear: () => dispatch("CLEAR_MESSAGE")
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ChatRoom);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,67 @@
-import updateAuth from "./updateAuth";
+const initialState = {
+  currentUser: null,
+  chat: [],
+  username: "",
+  pass: "",
+  fetching: false,
+  error: false,
+  message: ""
+};
+
+function addMessage(newMessage, list) {
+  if (newMessage.body === "") return list;
+  return [...list, newMessage];
+}
 
 const reducer = (state, action) => {
-  return {
-    auth: updateAuth(state, action)
-  };
+  if (state === undefined) {
+    return initialState;
+  }
+
+  switch (action.type) {
+    case "CHANGE_USERNAME": {
+      return {
+        ...state,
+        username: action.payload
+      };
+    }
+
+    case "CHANGE_PASS": {
+      return {
+        ...state,
+        pass: action.payload
+      };
+    }
+
+    case "CHANGE_MESSAGE": {
+      return {
+        ...state,
+        message: action.payload
+      };
+    }
+
+    case "CLEAR_MESSAGE": {
+      return {
+        ...state,
+        message: ""
+      };
+    }
+
+    case "POST_MESSAGE": {
+      const newMessage = {
+        body: state.message,
+        user: state.currentUser
+      };
+      return {
+        ...state,
+        chat: addMessage(newMessage, state.chat),
+        message: ""
+      };
+    }
+
+    default:
+      return state;
+  }
 };
 
 export default reducer;
